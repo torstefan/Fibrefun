22207ca17c4fc1cea75c20817c8120fbd791b299
Tor Stefan Lura <tofu@ihelse.net>
2014-07-29 20:38:33 +0200

    Second commit

--- Fibre-Fun/lib/Fibre/Fun.pm
+++ Fibre-Fun/lib/Fibre/Fun.pm
@@ -1,10 +1,36 @@
 package Fibre::Fun;
 use Dancer ':syntax';
+use Data::Dumper;
 
 our $VERSION = '0.1';
 
+
+
+
 get '/' => sub {
     template 'index';
+};
+
+get '/add_new_fibre_type' => sub {
+    if(defined(session('fibre_params'))){
+    	my $fp = session('fibre_params');
+    	info "get add_new_fibre_type";
+    	info Dumper($fp);
+    	template 'add_new_fibre_type', { hei => $fp };
+    	   	
+    }else{
+    	template 'add_new_fibre_type';
+    	
+    }
+    
+};
+
+post '/add_new_fibre_type' => sub {
+	my $fp = params;
+	session fibre_params => $fp;
+	info "post add_new_fibre_type";
+	info Dumper(session('fibre_params'));    
+    redirect '/add_new_fibre_type';
 };
 
 true;
